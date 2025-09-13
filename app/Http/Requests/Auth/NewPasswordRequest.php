692986c9303ddbc4b782def20198a075b81ec781
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2025-09-13 14:03:23 +0300

    Add NewPasswordRequest for password reset functionality
    - Introduced NewPasswordRequest to handle validation for new password requests.
    - Implemented validation rules for token, email, and password, ensuring required fields are validated.
    - Added rate limiting to enhance security and prevent abuse during password reset processes.

---
+++ app/Http/Requests/Auth/NewPasswordRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules\Password;
+
+class NewPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'token' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ];
+    }
+
+    /**
+     * Get the rate limiting throttle key for the request.
+     */
+    public function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->input('email')).'|'.$this->ip());
+    }
+}
