911a38fc89d24a421a83844e16c3957cdec8360f
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2025-09-14 20:55:32 +0300

    Add AuthServiceProvider for authorization management
    - Introduced AuthServiceProvider class to manage authorization policies for Board, Column, and Task models.
    - Registered policies to ensure proper access control and security for user actions on these models, enhancing the application's authorization framework.

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Board;
+use App\Models\Column;
+use App\Models\Task;
+use App\Policies\BoardPolicy;
+use App\Policies\ColumnPolicy;
+use App\Policies\TaskPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Board::class => BoardPolicy::class,
+        Column::class => ColumnPolicy::class,
+        Task::class => TaskPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
